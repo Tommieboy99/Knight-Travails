43475ce76baa06231d31c7f87979747dc61551fd
Tommieboy99 <tomkoek99@hotmail.com>
2025-11-05 11:48:13 +0100

    created a function that returns the shortest path using bfs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
-import './styles.css';
+import { createGraph } from './createGraph.js';
 
-console.log('Template for your new project!');
+const knightMovesGraph = createGraph();
+
+function knightMoves(graph, start, end) {
+  if (!isValidSquare(start) || !isValidSquare(end)) {
+    throw new Error('Please enter valid coordinate(s)');
+  }
+
+  const queue = [start];
+  const visited = [start];
+  const parent = {};
+
+  while (queue.length > 0) {
+    const current = queue.shift();
+
+    if (current === end) {
+      const path = [end];
+
+      while (path[0] !== start) {
+        const parentNode = parent[path[0]];
+        path.unshift(parentNode);
+      }
+
+      return path;
+    }
+
+    const neighbors = graph[current];
+
+    for (let neighbor of neighbors) {
+      const neighborKey = `${neighbor[0]},${neighbor[1]}`;
+
+      if (!visited.includes(neighborKey)) {
+        visited.push(neighborKey);
+        parent[neighborKey] = current;
+        queue.push(neighborKey);
+      }
+    }
+  }
+
+  return null;
+}
+
+function isValidSquare(coordinate) {
+  const regex = /^([0-7]),([0-7])$/;
+  const match = coordinate.match(regex);
+
+  return match !== null;
+}
+
+console.log(knightMoves(knightMovesGraph, '0,0', '5,5'));
